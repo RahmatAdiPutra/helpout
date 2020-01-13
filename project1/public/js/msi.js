888b714c8e9703031063c41023632b0bb13d4362
Aman <rahmat.adi.putra21@gmail.com>
2020-01-13 10:41:37 +0700

    Update msi.js

--- project1/public/js/msi.js
+++ project1/public/js/msi.js
@@ -11,7 +11,38 @@
         data.var.toolbar = '#detailedTable_wrapper .row .col-sm-12 .row .col-sm-12';
         return data.var;
     };
- 
+
+    $.auth = function(token) {
+        // var result = null;
+        // $.get($('base').attr('href') + '/api/employee/' + token).done(function (data){
+        //     result = data;
+        //     console.log(result);
+        // });
+        // return result;
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.open('GET', $('base').attr('href') + '/api/employee/' + token, false);
+        xhttp.send();
+        var a = xhttp;
+        return a;
+
+        // var test = function (token) {
+        //     var tmp = null;
+        //     $.ajax({
+        //         async: false,
+        //         method: 'GET',
+        //         dataType: 'json',
+        //         url:  $('base').attr('href') + '/api/employee/' + token,
+        //         success: function (response) {
+        //             tmp = response;
+        //         },
+        //         error: function (response) {}
+        //     });
+        //     return tmp;
+        // }();
+        // return test;
+    };
+
     $.fn.greenify = function() {
         this.css( "color", "green" );
         return this;
@@ -32,5 +63,5 @@
             return str;
         }
         return show;
-    }; 
+    };
 }(jQuery));
